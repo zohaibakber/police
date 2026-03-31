96dbd7471d67eb48125bace533df2a67a39bc2a5
zohaibakber <zohaib_akber@yahoo.com>
2026-04-01 01:28:45 +0500

    refactor: remove unused React import and update Spinner component props

--- src/components/ui/spinner.tsx
+++ src/components/ui/spinner.tsx
@@ -1,8 +1,8 @@
 import { cn } from "@/lib/utils"
-import { HugeiconsIcon } from "@hugeicons/react"
+import { HugeiconsIcon, HugeiconsIconProps } from "@hugeicons/react"
 import { Loading03Icon } from "@hugeicons/core-free-icons"
 
-function Spinner({ className, ...props }: React.ComponentProps<"svg">) {
+function Spinner({icon, className, ...props }: HugeiconsIconProps) {
   return (
     <HugeiconsIcon icon={Loading03Icon} strokeWidth={2} role="status" aria-label="Loading" className={cn("size-4 animate-spin", className)} {...props} />
   )
